1a7e4b576ef4cd15139edff4b9a6dbc181d1de36
MartinPC <MartinChrisPC@hotmail.com>
2024-08-05 23:21:07 -0500

    feat: completando 4 retos - ConteoPalabras - DecimalToBinario - InversorCadenas - Morse

--- src/main/java/inversorcadenas/InversorCadenas.java
+++ src/main/java/inversorcadenas/InversorCadenas.java
@@ -8,6 +8,15 @@ package inversorcadenas;
 public class InversorCadenas {
 
   public static void main(String[] args) {
+    String palabra = "orellana";
+    String reversePalabra = "";
+    //StringBuilder reversePalabra = new StringBuilder();
 
+    for (int i = palabra.length() - 1; i >= 0; i--) {
+      //reversePalabra.append(palabra.charAt(i));
+      reversePalabra += palabra.charAt(i);
+    }
+
+    System.out.println(reversePalabra);
   }
 }
